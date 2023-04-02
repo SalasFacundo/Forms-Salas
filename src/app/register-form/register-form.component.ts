9b4a332ebc6a80e754370de2e5af0d474704000e
Facundo <salasfacundo96@gmail.com>
2023-04-02 01:11:51 -0300

    Se agergaron los componentes de navbar y de formulario de registro

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -13,11 +13,12 @@ export class RegisterFormComponent implements OnInit {
   formulario = new FormGroup({
     userName : new FormControl('', [Validators.required, Validators.minLength(4)]),
     userLastName : new FormControl('', [Validators.required, Validators.minLength(4)]),
-    country : new FormControl('', [Validators.required, Validators.minLength(4)]),
-    province : new FormControl('', [Validators.required, Validators.minLength(4)]),
+    country : new FormControl('', [Validators.required, Validators.minLength(2)]),
+    province : new FormControl('', [Validators.required, Validators.minLength(3)]),
     city : new FormControl('', [Validators.required, Validators.minLength(4)]),
     email : new FormControl('', [Validators.required, Validators.email, Validators.minLength(4)])
   });
+  
   ngOnInit(): void {
   }
 
